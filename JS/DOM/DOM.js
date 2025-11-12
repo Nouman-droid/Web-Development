d5e6fe0fc96fd739572bd7d454873384f7caf3c2
Syed Nouman Ahmed <syednoumanahmed2001@gmail.com>
2025-11-12 22:01:16 +0530

    .

---
+++ JS/DOM/DOM.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+document.body.childNodes[0];
+document.body.childNodes[1];
+document.body.childNodes[2];
+document.body.childNodes[3];
+document.body.childNodes[4];
+
+// let cont = document.body.childNodes[1];
+
+// cont.firstChild;
+// cont.lastChild;
+// cont.firstElementChild;
+// cont.lastElementChild;
+
+document.body.firstElementChild.children[0];
+document.body.firstElementChild.children[1];
+document.body.firstElementChild.children[2];
+document.body.firstElementChild.children[3];
+document.body.firstElementChild.children[4];
+document.body.firstElementChild.children[5];
+
+
+document.body.firstElementChild.children[1].nextElementSibling;
+
+document.body.firstElementChild.children[1].previousSibling;
+
+document.body.firstElementChild.children[1].nextSibling;
+
+document.body.firstElementChild.children[1].previousElementSibling;
